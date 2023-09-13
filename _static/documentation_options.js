557c26757ba85610258ff080a93201037972c6d6
Moisès Gómez <moises.gomez@iesjoandaustria.org>
2023-09-13 18:41:09 +0200

    automated commit on dimecres, 13 de setembre de 2023, 18:41:09 CEST

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2022-23',
+    VERSION: '2023-24',
     LANGUAGE: 'ca',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
